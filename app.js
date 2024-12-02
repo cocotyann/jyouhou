7cf7fd5fa3caf8556e0e744c1873f5201a9a2b5b
cocotyann <24HS114@s.bgu.ac.jp>
2024-12-02 14:07:42 +0900

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,20 @@
                 result = "動物園";
             } else if (colour === "晴れ" && number === スキッリ気分) {
                 result = "遊園地";
-            } else if (colour === "晴れ" && number === ) {
+            } else if (colour === "晴れ" && number === 目の保養  function checkFortune() {
+            var colour = document.getElementById('colour-select').value;
+            var number = parseInt(document.getElementById('number-input').value, 10);
+            var result = "凶"; // デフォルトの値
+
+            if (colour === "晴れ" && number === 体を動かす気分) {
+                result = "公園";
+            } else if (colour === "晴れ" && number === のんびり気分）｛　　
+            　　result = "温泉";
+　　　　　　　｝else if (colour === "晴れ" && number === 癒し気分) {
+                result = "動物園";
+            } else if (colour === "晴れ" && number === スキッリ気分) {
+                result = "遊園地";
+            } else if (colour === "晴れ" && number === 目の保養) {
                 result = "";
             } else if (colour === "雨" && number === 家で過ごす気分) {
                 result = "ドラマ鑑賞　読書";
@@ -21,7 +34,8 @@
                 result = "地域のスポーツセンター";
             } else if (colour === "雨" && number ===のんびり気分 ) {
                 result = "お家でゴロゴロ";
-            }
+            }else if (colour === "雨" && number ===スキッリ ) {
+                result = "ネットで買い物";
 
             document.getElementById('result-output').innerText = "君のお出かけ先は【" + result + "】です";
         }
