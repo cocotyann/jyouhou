0026a9135d5077e77b895ec6b98fa220e53592a6
cocotyann <24HS114@s.bgu.ac.jp>
2024-12-20 15:10:28 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
       function checkFortune() {
             var feeling = document.getElementById('feeling-select').value;
-            var number = parseInt(document.getElementById('number-input').value, 2);
+            var number = parseInt(document.getElementById('number-input').value, 10);
             var result =″おでかけ先″; // デフォルトの値
 
             if (feeling === ″体を動かす気分″ && number === 1) {
                 result = ″公園″;
             } else if (feeling === ″のんびり気分″ && number  === 1）｛　　
             　　result = ″温泉″;
-　　　　　　　｝else if (feeling === ″癒し気分″ && number === 1) {
+　　　　　　 ｝else if (feeling === ″癒し気分″ && number === 1) {
                 result = ″動物園″;
             } else if (feeling === ″スキッリ気分″ && number === 1) {
                 result = ″遊園地″;
-           } else if (feeling === ″家で過ごす気分″ && number === 1) {
+            } else if (feeling === ″家で過ごす気分″ && number === 1) {
                 result = ″ドラマ鑑賞　読書″;
             } else if (feeling === ″癒し気分″ && number === 2) {
                 result = ″水族館″;
