5843c0c68044d25e20f851bfc776380773bc59e2
cocotyann <24HS114@s.bgu.ac.jp>
2024-12-23 13:34:57 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
                 result = "公園";
             } else if (feeling === "体を動かす気分" && number === 2) {
                 result = "地域のスポーツセンター";
-            }  else if (feeling === "のんびり気分" && number  === 1){　　
+            }  else if (feeling === "のんびり気分" && number  === 1) {　　
             　　result = "温泉";
             } else if (feeling === "のんびり気分" && number === 2) {
                 result = "お家でゴロゴロ";
@@ -17,11 +17,11 @@
                 result = "水族館";
             } else if (feeling === "スッキリ気分" && number === 1) {
                 result = "遊園地";
-            } else if (feeling === "スッキリ気分" && number === 2){ 　
+            } else if (feeling === "スッキリ気分" && number === 2) { 　
                   result = "ネットで買い物　部屋の掃除";
-            } else if (feeling ==="家で過ごす気分" && number === 1){
+            } else if (feeling ==="家で過ごす気分" && number === 1) {
                 result = "ドラマ鑑賞　読書";
-            } else if (feeling ==="家で過ごす気分" && number === 2){
+            } else if (feeling ==="家で過ごす気分" && number === 2) {
                 result = "料理";
             } 
            document.getElementById('result-output').innerText = "君のお出かけ先は【" + result + "】です";
